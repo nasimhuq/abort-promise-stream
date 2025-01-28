fdc089ce3d3232b4f939b6861a445a1b64ca88b3
nasimhuq <nasim.huq@gmail.com>
2025-01-27 21:34:51 -0500

    Fixed memory leak in observableArray

--- src/observableArray.js
+++ src/observableArray.js
@@ -1,53 +1,53 @@
 export function observableArray(array) {
-    const listeners = new Set()
+  const listeners = new Set()
 
-    const proxy = new Proxy(array, {
-        set: (target, property, value) => {
-            const success = Reflect.set(target, property, value)
-            if (success) {
-                notifyListeners()
-            }
-            return success
-        },
-        get: (target, property) => {
-            if (property === 'push') {
-                return (...args) => {
-                    target.push(...args)
-                    notifyListeners()
-                    return target.length
-                }
-            }
-            if (['pop', 'shift', 'unshift'].find((prop) => prop === property)) {
-                return () => {
-                    const result = target[property]()
-                    notifyListeners()
+  const proxy = new Proxy(array, {
+    set: (target, property, value) => {
+      const success = Reflect.set(target, property, value)
+      if (success) {
+        notifyListeners()
+      }
+      return success
+    },
+    get: (target, property) => {
+      if (property === 'push') {
+        return (...args) => {
+          target.push(...args)
+          notifyListeners()
+          return target.length
+        }
+      }
+      if (['pop', 'shift', 'unshift'].find((prop) => prop === property)) {
+        return () => {
+          const result = target[property]()
+          notifyListeners()
 
-                    return result
-                }
-            }
-            return Reflect.get(target, property)
-        },
+          return result
+        }
+      }
+      return Reflect.get(target, property)
+    },
+  })
+
+  function notifyListeners() {
+    listeners.forEach((listener) => {
+      listener(array)
     })
+  }
+  function subscribe(listener) {
+    listeners.add(listener)
+  }
+  function unsubscribeAll() {
+    listeners.clear()
+  }
+  function unsubscribe(listener) {
+    listeners.delete(listener)
+  }
 
-    function notifyListeners() {
-        listeners.forEach((listener) => {
-            listener(array)
-        })
-    }
-    function subscribe(listener) {
-        listeners.add(listener)
-    }
-    function unsubscribeAll() {
-        while (listeners.length) listeners.pop()
-    }
-    function unsubscribe(listener) {
-        listeners.delete(listener)
-    }
-
-    return {
-        value: proxy,
-        subscribe,
-        unsubscribe,
-        unsubscribeAll,
-    }
+  return {
+    value: proxy,
+    subscribe,
+    unsubscribe,
+    unsubscribeAll,
+  }
 }
